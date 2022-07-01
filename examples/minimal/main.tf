7901472ac15878da7864a113b7277c1e72fea24e
Sindre Lindstad <>
2022-07-01 14:03:26 +0200

    chore(docs): Releases path

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,7 +1,7 @@
 module "init" {
   # This is an example only; if you're adding this block to a live configuration,
   # make sure to use the latest release of the init module, found here:
-  # https://github.com/entur/terraform-google-init
+  # https://github.com/entur/terraform-google-init/releases
   source      = "github.com/entur/terraform-google-init//modules/init?ref=v0.1.0"
   app_id      = "tfmodules"
   environment = "dev"
--- examples/minimal_test/main.tf
+++ examples/minimal_test/main.tf
@@ -8,7 +8,7 @@ resource "random_integer" "random_database_generation" {
 module "init" {
   # This is an example only; if you're adding this block to a live configuration,
   # make sure to use the latest release of the init module, found here:
-  # https://github.com/entur/terraform-google-init
+  # https://github.com/entur/terraform-google-init/releases
   source      = "github.com/entur/terraform-google-init//modules/init?ref=v0.1.0"
   app_id      = "tfmodules"
   environment = "dev"
