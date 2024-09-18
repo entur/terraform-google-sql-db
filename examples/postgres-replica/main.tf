16dbf35ec847fbf28878f8a2249682d44c7b98e4
Kristian Nilsen <kristian.nilsen@entur.org>
2024-09-18 08:53:06 +0200

    Fix typo in module postgresql in test data for postgres-replica

--- examples/postgres-replica/main.tf
+++ examples/postgres-replica/main.tf
@@ -29,7 +29,7 @@ module "postgresql" {
     memory = 3840
   }
   additional_users = {
-    user1 = { username = "user1", crea  te_kubernetes_secret = false },
+    user1 = { username = "user1", create_kubernetes_secret = false },
     user2 = { username = "user2", create_kubernetes_secret = false }
   }
 }
