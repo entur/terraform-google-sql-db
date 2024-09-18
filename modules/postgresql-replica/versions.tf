b92fbc0f726245787cf3122c35424ddff7cc1061
Kristian Nilsen <kristian.nilsen@entur.org>
2024-09-18 10:13:55 +0200

    Add deletion_protection_enabled property to postgresql-replica test data

--- modules/postgresql-replica/versions.tf
+++ modules/postgresql-replica/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">=5.44.0"
+      version = ">=4.26.0"
     }
   }
   required_version = ">=0.13.2"
--- modules/postgresql/versions.tf
+++ modules/postgresql/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     google = {
       source  = "hashicorp/google"
-      version = ">=5.44.0"
+      version = ">=4.26.0"
     }
     random = {
       source  = "hashicorp/random"
